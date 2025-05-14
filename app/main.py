93fc8e6e8f7770e33410042996ae0cbc8cd99360
Rituraj <cierituraj@gmail.com>
2025-05-14 10:14:43 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
 import sys
 
-
 def main():
-    # Uncomment this block to pass the first stage
-     sys.stdout.write("$ ")
+    # Print the prompt
+    sys.stdout.write("$ ")
+    sys.stdout.flush()  # Ensure the prompt is displayed immediately
 
     # Wait for user input
     command = input()
     if command == "exit 0":
         sys.exit(0)
-    else:
     print(f"{command}: command not found")
     main()
 
-
 if __name__ == "__main__":
     main()
+
