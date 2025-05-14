1fcf59e33ba6a64a1e37be9dcd08a1aa256fd917
Rituraj <cierituraj@gmail.com>
2025-05-14 10:06:48 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,8 +7,9 @@ def main():
 
     # Wait for user input
     command = input()
+    if command == "exit 0":
+        return exit(0)
     print(f"{command}: command not found")
-    print("exit 0")
     main()
 
 
